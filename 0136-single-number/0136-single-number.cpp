0ba5c631cbddb702777aef1e18bdd22afa356490
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-02-28 14:01:44 +0530

    Time: 0 ms (100%), Space: 20.6 MB (57.95%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-    sort(nums.begin(),nums.end());
-    int i =0;
-    while (i<nums.size()-1)
-    {
-        if ((nums[i]^nums[i+1])==0)
-        {
-            i+=2;
-
-        }else{
-            return nums[i];
-        }
-        
+    int ans =0;
+    for(int x: nums){
+        ans ^=x;
     }
-
-    return nums[nums.size()-1];
+    return ans;
     }
 };
