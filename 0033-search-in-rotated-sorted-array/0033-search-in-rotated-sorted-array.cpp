474aed36ae2fb466bb8f3c0efab823cc396d3b12
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-03-21 07:35:40 +0530

    Time: 0 ms (100%), Space: 15.3 MB (34.64%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        return search(nums, 0, nums.size() - 1, target);
+        return binSearch(nums, 0, nums.size()-1, target);
+
     }
 
-    int search(vector<int> nums, int si, int ei, int target) {
-        if (si > ei) return -1;
+    int binSearch(vector<int>& nums, int si, int ei, int target){
+        if(si>ei){
+            return -1;
+        }
 
-        int mid = si + (ei - si) / 2;
+        int mid = si+(ei-si)/2;
 
-        if (nums[mid] == target) {
+        if(nums[mid]== target){
             return mid;
         }
 
-        if (nums[si] <= nums[mid]) {
-            if (nums[si] <= target && target <= nums[mid]) {
-                return search(nums, si, mid - 1, target);
-            } else {
-                return search(nums, mid + 1, ei, target);
+        if(nums[si]<=nums[mid]){
+            if(nums[si]<= target && target<nums[mid]){
+                return binSearch(nums, si, mid-1, target);
+            }else{
+                return binSearch(nums, mid+1, ei, target);
             }
-        } else {
-            if (nums[mid] <= target && target <= nums[ei]) {
-                return search(nums, mid + 1, ei, target);
-            } else {
-                return search(nums, si, mid - 1, target);
+        }else{
+            if(nums[mid]<=target && target<=nums[ei]){
+                return binSearch(nums, mid+1, ei, target);
+            }else{
+                return binSearch(nums, si, mid-1, target);
             }
         }
     }
