bcb7c2cc64cf574ebf35bb56b8d77f0bba15f614
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-06-29 00:14:47 +0530

    Time: 0 ms (100%), Space: 8.5 MB (50.33%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,33 @@
 class Solution {
 public:
-    int solve(int n, vector<int>& dp){
+
+    int tabulation(int n) {
+
+        // 1. Create DP array
+        vector<int> dp(n+1, -1);
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        // 3. Fill the table
+        for (int i = 2; i <= n; i++) {
+
+            // Transition
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        // 4. Return final answer
+        return dp[n];
+    }
+
+    int memoization(int n, vector<int>& dp){
         if(n == 0 || n == 1) return 1;
 
         if(dp[n]!=-1){
             return dp[n];
         }
 
-        int ans = solve(n-1, dp)+solve(n-2, dp);
+        int ans = memoization(n-1, dp)+memoization(n-2, dp);
 
         return dp[n] = ans;
     }
@@ -16,6 +36,6 @@ public:
     int climbStairs(int n) {
         vector<int> dp(n+1, -1);
         
-        return solve(n, dp);
+        return tabulation(n);
     }
 };
