4c895c7ce9216c8673e1adc20716d84fe33afaa2
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-06-30 01:07:13 +0530

    Time: 0 ms (100%), Space: 17.8 MB (10.16%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
 
+    int tabulation(vector<int>& cost, int n){
+        vector<int> dp(n);
+
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+
+        for(int i = 2; i<n ; i++){
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+        }
+
+        return min(dp[n-1],dp[n-2]);
+    }
+
     int solve(vector<int>& cost, int i, vector<int>& dp){
         if(i == 0 || i==1) return cost[i];
 
@@ -16,6 +29,8 @@ public:
         int n = cost.size();
 
         vector<int> dp(n,-1);
-        return min(solve(cost, n-1, dp), solve(cost, n-2, dp));
+        // return min(solve(cost, n-1, dp), solve(cost, n-2, dp));
+
+        return tabulation(cost, n);
     }
 };
