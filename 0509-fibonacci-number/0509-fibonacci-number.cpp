8a6f2e231fd4b99f7187966d0b3e716a1ff2bbbf
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-02 23:56:16 +0530

    Time: 3 ms (65.35%), Space: 8.2 MB (16.24%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
-    int fib(int n) {
+
+    int solve(int n, vector<int>& dp){
         if(n==0 || n==1) return n;
 
-        return fib(n-1)+fib(n-2);
+        if(dp[n] != -1) return dp[n];
+
+        int ans = solve(n-1, dp)+solve(n-2, dp);
+
+        return dp[n] = ans;
+
+    }
+
+    int fib(int n) {
+       
+        vector<int> dp(n+1, -1);
+
+        return solve(n, dp);
     }
 };
