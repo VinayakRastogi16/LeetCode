35514bf374f407420008afa3de58b145feb3369b
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-02 23:45:53 +0530

    Time: 26 ms (63.57%), Space: 17.8 MB (83.64%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -20,7 +20,7 @@ public:
 
     }
 
-    int coinChange(vector<int>& coins, int amount) {
+    int coinChange2(vector<int>& coins, int amount) {
         if(amount == 0) return 0;
 
         vector<int> dp(amount+1, -1);
@@ -35,4 +35,24 @@ public:
 
         return ans;
     }
+
+    int coinChange(vector<int>& coins, int amount) {
+
+    vector<int> dp(amount + 1, INT_MAX);
+
+    dp[0] = 0;
+
+    for(int i = 1; i <= amount; i++) {
+
+        for(int j = 0; j < coins.size(); j++) {
+
+            if(i >= coins[j] && dp[i - coins[j]] != INT_MAX) {
+
+                dp[i] = min(dp[i], 1 + dp[i - coins[j]]);
+            }
+        }
+    }
+
+    return (dp[amount] == INT_MAX) ? -1 : dp[amount];
+}
 };
