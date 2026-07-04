448ef268b6610becd0c1a4545a98028c50fa6eb1
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-04 23:53:46 +0530

    Time: 47 ms (22.62%), Space: 61.7 MB (8.09%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,6 +1,43 @@
 class Solution {
 public:
 
+    // const long long MOD = 1e9+7;
+
+    int tabulation(int amount, vector<int>& coins){
+        int n = coins.size();
+
+        vector<vector<long long>> dp(n+1, vector<long long>(amount+1,0));
+
+
+        for(int i = 0; i<=n; i++){
+            dp[i][0] = 1;
+
+        }
+
+         const long long CAP = 4e18;
+
+        for(int i = n-1; i>=0; i--){
+            for(int amt = 1; amt<=amount; amt++){
+                long long take = 0;
+
+                if(amt>=coins[i]){
+                    take = dp[i][amt - coins[i]];
+                }
+
+                long long notTake = dp[i+1][amt];
+
+                if(take > CAP - notTake){
+                    dp[i][amt] = CAP;
+                } else {
+                    dp[i][amt] = take + notTake;
+                }
+            }
+
+        }
+
+        return (int)dp[0][amount];
+    }
+
     int solve(int i, int amount, vector<int>& coins, vector<vector<int>>& dp){
         if(amount == 0) return 1;
 
@@ -22,8 +59,10 @@ public:
     }
 
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size()+1, vector<int>(amount+1,-1));
+        // vector<vector<int>> dp(coins.size()+1, vector<int>(amount+1,-1));
 
-        return solve(0, amount, coins, dp);
+        // return solve(0, amount, coins, dp);
+
+        return tabulation(amount, coins);
     }
 };
