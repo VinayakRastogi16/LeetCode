ecbfb4f6e313ce68c85949bf70af2c6f5cf178a2
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-05 23:38:47 +0530

    Time: 31 ms (37.39%), Space: 45.3 MB (5.94%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
 
+    int tabulation(const string& t1, const string&t2){
+
+        int n = t1.size(), m = t2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = m - 1; j >= 0; j--) {
+
+                if(t1[i] == t2[j]) {
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                }
+                else {
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+
     int solve(int i, int j, const string& t1, const string& t2, vector<vector<int>>& dp){
         if(i==t1.size() || j == t2.size())return 0;
 
@@ -15,9 +35,11 @@ public:
 
     int longestCommonSubsequence(string text1, string text2) {
 
-        vector<vector<int>> dp(text1.size()+1, vector<int>(text2.size(), -1));
+        vector<vector<int>> dp(text1.size()+1, vector<int>(text2.size()+1, -1));
 
-        return solve(0,0, text1, text2, dp);
+        // return solve(0,0, text1, text2, dp);
+
+        return tabulation(text1, text2);
         
     }
 };
