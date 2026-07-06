172e4562e505414d331848d2cf4ae47f6a72a370
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-07 00:07:18 +0530

    Time: 12 ms (19.93%), Space: 16 MB (10.13%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,6 +1,39 @@
 class Solution {
 public:
 
+    int tabulation(const string& w1, const string& w2){
+        int n = w1.size();
+        int m = w2.size();
+        vector<vector<int>> dp(w1.size()+1, vector<int>(w2.size()+1,0));
+
+        for(int i = 0; i<=n; i++){
+            dp[i][m] = n-i;
+        }
+
+        for(int j = 0; j<=m; j++){
+            dp[n][j] = m-j;
+        }
+
+
+        for(int i = n-1; i>=0; i--){
+            for(int j = m-1; j>=0; j--){
+                if(w1[i]==w2[j]) {
+                    dp[i][j] = dp[i+1][j+1];
+                }else{
+                    int insert = 1+dp[i][j+1];
+                    int replace = 1+dp[i+1][j+1];
+                    int del = 1+dp[i+1][j];
+                    
+
+                    dp[i][j] = min({insert, replace, del});
+
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+
     int solve(int i, int j, const string& w1, const string& w2, vector<vector<int>>& dp){
 
         if(i == w1.size()) return w2.size()-j;
@@ -24,6 +57,7 @@ public:
 
         vector<vector<int>> dp(word1.size()+1, vector<int>(word2.size()+1, -1));
 
-        return solve(0, 0, word1, word2, dp);
+        // return solve(0, 0, word1, word2, dp);
+        return tabulation(word1, word2);
     }
 };
