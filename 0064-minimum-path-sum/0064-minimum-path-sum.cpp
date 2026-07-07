476beb47f68db11b4ca40f0bb32524f65aadf29e
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-07 22:38:16 +0530

    Time: 0 ms (100%), Space: 17 MB (16.59%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
 
+    int tabulation(vector<vector<int>>& grid){
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<int>> dp(m+1, vector<int>(n+1, INT_MAX));
+        dp[m-1][n-1] = grid[m-1][n-1];
+
+        for(int i = m-1; i>=0; i--){
+            for(int j = n-1; j>=0; j--){
+                if(i== m-1&& j==n-1) continue;
+                int down = dp[i+1][j];
+                int right = dp[i][j+1];
+
+                int best = min(down, right);
+
+                if(best == INT_MAX)
+                    dp[i][j] = INT_MAX;
+                else
+                    dp[i][j] = grid[i][j] + best;
+
+                
+            }
+        }
+
+        return dp[0][0];
+
+
+    }
+
     int solve(vector<vector<int>>& grid, int i, int j, vector<vector<int>>& dp){
 
         int m = grid.size();
@@ -24,6 +53,8 @@ public:
 
     int minPathSum(vector<vector<int>>& grid) {
         vector<vector<int>> dp(grid.size()+1, vector<int>(grid[0].size()+1, -1));
-        return solve(grid, 0, 0, dp);
+        // return solve(grid, 0, 0, dp);
+
+        return tabulation(grid);
     }
 };
