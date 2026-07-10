44f5ae48e594039982c2e2eaf768baba14bb8d57
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-10 23:15:38 +0530

    Time: 387 ms (25.58%), Space: 576.4 MB (6.52%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
 
+    int tabulation(vector<int>& nums){
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+
+        
+
+        for(int i = n-1; i>=0; i--){
+            for(int j = i-1; j>=-1; j--){
+                int take = 0;
+
+                if(j ==-1 ||nums[i]>nums[j])
+                take = 1 + dp[i+1][i+1];
+
+                int notTake = dp[i+1][j+1];
+
+                dp[i][j+1] = max(take, notTake);
+            }
+        }
+
+        return dp[0][0];
+    }
+
     int solve(int i, int prevIdx, vector<int>& nums, vector<vector<int>>& dp){
 
         if(i == nums.size())
@@ -23,6 +45,7 @@ public:
 
         vector<vector<int>> dp(nums.size(), vector<int>(nums.size()+1, -1));
 
-        return solve(0, -1, nums, dp);
+        // return solve(0, -1, nums, dp);
+        return tabulation(nums);
     }
 };
