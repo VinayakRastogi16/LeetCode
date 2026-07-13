6ebdf333fa6f27912c2e091130f373413cb9f787
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-14 00:30:56 +0530

    Time: 12 ms (41.17%), Space: 31.1 MB (40.6%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,5 +1,25 @@
 class Solution {
 public:
+
+    int tabulation(vector<vector<char>>& matrix){
+        int m = matrix.size(), n = matrix[0].size();
+
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+
+        int maxSide = 0;
+
+        for(int i = m-1; i>=0; i--){
+            for(int j = n-1; j>=0; j--){
+                if(matrix[i][j] == '0') dp[i][j]=0;
+                else
+                    dp[i][j] = 1 + min({dp[i+1][j], dp[i][j+1], dp[i+1][j+1]});
+                
+                maxSide = max(maxSide, dp[i][j]);
+            }
+        }
+
+        return maxSide* maxSide;
+    }
     
     int solve(int i, int j, vector<vector<char>>& m, vector<vector<int>>& dp){
 
@@ -16,20 +36,20 @@ public:
 
     int maximalSquare(vector<vector<char>>& matrix) {
         
-        int m = matrix.size(), n = matrix[0].size();
+        // int m = matrix.size(), n = matrix[0].size();
 
-        int maxSide = 0;
+        // int maxSide = 0;
 
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        // vector<vector<int>> dp(m, vector<int>(n, -1));
 
-        for(int i = 0; i<m;i++){
-            for(int j = 0; j<n; j++){
-                maxSide = max(maxSide, solve(i, j, matrix, dp));
-            }
-        }
+        // for(int i = 0; i<m;i++){
+        //     for(int j = 0; j<n; j++){
+        //         maxSide = max(maxSide, solve(i, j, matrix, dp));
+        //     }
+        // }
 
+        // return maxSide* maxSide;
 
-
-        return maxSide* maxSide;
+        return tabulation(matrix);
     }
 };
