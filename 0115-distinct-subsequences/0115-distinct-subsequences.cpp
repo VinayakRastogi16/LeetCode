91bec4a1430b2b0cdaad2488452afbaf80f2561c
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-15 00:00:37 +0530

    Time: 43 ms (12.86%), Space: 79.3 MB (9.38%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
 
-    long long tabulation(const string s, const string& t){
+    unsigned long long tabulation(const string s, const string& t){
         int m = s.size(), n = t.size();
 
-        vector<vector<long long>> dp(m+1, vector<long long>(n+1, 0));
+        vector<vector<unsigned long long>> dp(m+1, vector<unsigned long long>(n+1, 0));
         for(int i = 0; i<=m; i++){
             dp[i][n] = 1;
         }
@@ -12,8 +12,7 @@ public:
 
         for(int i = m-1; i>=0; i--){
             for(int j = n-1; j>=0; j--){
-                const long long LIM = INT_MAX;
-                if(s[i]==t[j]) dp[i][j] = min(LIM,dp[i+1][j]+dp[i+1][j+1]);
+                if(s[i]==t[j]) dp[i][j] = dp[i+1][j]+dp[i+1][j+1];
                 else dp[i][j] = dp[i+1][j];
 
             }
@@ -36,6 +35,6 @@ public:
     int numDistinct(string s, string t) {
         vector<vector<long long>> dp(s.size(), vector<long long>(t.size(), -1));
         // return solve(0, 0, s, t, dp);
-        return (int)tabulation(s, t);
+        return static_cast<int>(tabulation(s, t));
     }
 };
