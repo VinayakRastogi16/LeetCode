8d02f723fdfa7fc7dbbab8c730b8d6121386333e
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-14 23:54:42 +0530

    Time: 46 ms (11.13%), Space: 79.1 MB (9.4%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,7 +1,28 @@
 class Solution {
 public:
 
-    int solve(int i, int j, const string& s, const string& t, vector<vector<long long>>& dp){
+    long long tabulation(const string s, const string& t){
+        int m = s.size(), n = t.size();
+
+        vector<vector<long long>> dp(m+1, vector<long long>(n+1, 0));
+        for(int i = 0; i<=m; i++){
+            dp[i][n] = 1;
+        }
+
+
+        for(int i = m-1; i>=0; i--){
+            for(int j = n-1; j>=0; j--){
+                const long long LIM = INT_MAX;
+                if(s[i]==t[j]) dp[i][j] = min(LIM,dp[i+1][j]+dp[i+1][j+1]);
+                else dp[i][j] = dp[i+1][j];
+
+            }
+        }
+
+        return dp[0][0];
+    }
+
+    long long solve(int i, int j, const string& s, const string& t, vector<vector<long long>>& dp){
         if(j == t.size()) return 1;
         if(i == s.size()) return 0;
 
@@ -14,6 +35,7 @@ public:
 
     int numDistinct(string s, string t) {
         vector<vector<long long>> dp(s.size(), vector<long long>(t.size(), -1));
-        return solve(0, 0, s, t, dp);
+        // return solve(0, 0, s, t, dp);
+        return (int)tabulation(s, t);
     }
 };
