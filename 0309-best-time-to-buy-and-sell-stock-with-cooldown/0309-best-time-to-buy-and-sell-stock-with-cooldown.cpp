d8e618dec7ba197ea5b530bbb0c6fe1ebd94dd02
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-15 23:08:46 +0530

    Time: 1 ms (45.3%), Space: 16.6 MB (9.42%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
 
+    int tabulation(vector<int>& p){
+        int n = p.size();
+
+        vector<vector<int>> dp(n+2, vector<int>(2, 0));
+
+        for(int i = n-1; i>=0; i--){
+            dp[i][1] = max(-p[i]+dp[i+1][0], dp[i+1][1]);
+
+            dp[i][0] = max(dp[i+2][1]+p[i], dp[i+1][0]);
+        }
+
+        return dp[0][1];
+
+    }
+
     int solve(int i, bool canBuy, vector<int>& p, vector<vector<int>>& dp){
         if(i >= p.size()) return 0;
 
@@ -24,6 +39,7 @@ public:
     int maxProfit(vector<int>& prices) {
         bool canBuy = 1;
         vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
-        return solve(0, canBuy, prices, dp);
+        // return solve(0, canBuy, prices, dp);
+        return tabulation(prices);
     }
 };
