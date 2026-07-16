42ad34d508b898215934f590ae8ce5a3ed70c628
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-17 00:09:13 +0530

    Time: 0 ms (100%), Space: 13.8 MB (5.1%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,5 +1,24 @@
 class Solution {
 public:
+
+    int tabulation(vector<vector<int>>& t){
+        int n = t.size();
+
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for(int i = 0; i<n; i++){
+            dp[n-1][i] = t[n-1][i];
+        }
+
+        for(int i = n-2; i>=0; i--){
+            for(int j = 0; j <= i; j++){
+                dp[i][j] = t[i][j]+min(dp[i+1][j], dp[i+1][j+1]);
+            }
+        }
+
+        return dp[0][0];
+    }
+
     int solve(int i, int j, vector<vector<int>>& triangle,
               vector<vector<int>>& dp) {
 
@@ -17,6 +36,7 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
-        return solve(0, 0, triangle, dp);
+        // return solve(0, 0, triangle, dp);
+        return tabulation(triangle);
     }
 };
