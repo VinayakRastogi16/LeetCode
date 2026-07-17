eb46a59b3936db38f3daf481b177926bb1f6b2fc
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-17 23:41:56 +0530

    Time: 0 ms (100%), Space: 14.2 MB (97.07%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -2,6 +2,30 @@ class Solution {
 public:
     static constexpr int INF = 1000000000;
 
+    int tabulation2(vector<vector<int>>& m){
+        int n = m.size();
+
+        for(int i = n-2; i>=0; i--){
+            for(int j = 0; j<n; j++){
+
+                int ans = m[i][j] + m[i+1][j];
+
+                if(j>0) ans = min(ans, m[i][j]+m[i+1][j-1]);
+
+                if(j<n-1) ans = min(ans, m[i][j]+m[i+1][j+1]);
+
+                m[i][j] = ans;
+            }
+
+        }
+
+        int ans = INF;
+        for(int j = 0; j < n; j++)
+        ans = min(ans, m[0][j]);
+
+        return ans;
+    }
+
     int tabulation(vector<vector<int>>& m){
         int n = m.size();
 
@@ -10,7 +34,7 @@ public:
         vector<vector<int>> dp(n, vector<int>(n,0));
 
         for(int j = 0; j<n; j++){
-            dp[n-1][j] = m[n-1][j];
+           dp[n-1][j] = m[n-1][j];
         }
 
         for(int i = n-2; i>=0; i--){
@@ -34,35 +58,37 @@ public:
         return ans;
     }
 
-    int solve(int i, int j, vector<vector<int>>& m, vector<vector<int>>& dp) {
-        int n = m.size();
+    //Memoization
 
-        if (j < 0 || j >= n)
-            return INF;
+    // int solve(int i, int j, vector<vector<int>>& m, vector<vector<int>>& dp) {
+    //     int n = m.size();
 
-        if (dp[i][j] != INF)
-            return dp[i][j];
+    //     if (j < 0 || j >= n)
+    //         return INF;
 
-        if (i == n - 1)
-            return dp[i][j] = m[i][j];
+    //     if (dp[i][j] != INF)
+    //         return dp[i][j];
 
-        int left  = solve(i + 1, j - 1, m, dp);
-        int down  = solve(i + 1, j,     m, dp);
-        int right = solve(i + 1, j + 1, m, dp);
+    //     if (i == n - 1)
+    //         return dp[i][j] = m[i][j];
 
-        return dp[i][j] = m[i][j] + min(left, min(down, right));
-    }
+    //     int left  = solve(i + 1, j - 1, m, dp);
+    //     int down  = solve(i + 1, j,     m, dp);
+    //     int right = solve(i + 1, j + 1, m, dp);
+
+    //     return dp[i][j] = m[i][j] + min(left, min(down, right));
+    // }
 
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
 
-        vector<vector<int>> dp(n, vector<int>(n, INF));
+        // vector<vector<int>> dp(n, vector<int>(n, INF));
 
-        int ans = INF;
-        for (int j = 0; j < n; j++)
-            ans = min(ans, solve(0, j, matrix, dp));
+        // int ans = INF;
+        // for (int j = 0; j < n; j++)
+        //     ans = min(ans, solve(0, j, matrix, dp));
 
         // return ans;
-        return tabulation(matrix);
+        return tabulation2(matrix);
     }
 };
