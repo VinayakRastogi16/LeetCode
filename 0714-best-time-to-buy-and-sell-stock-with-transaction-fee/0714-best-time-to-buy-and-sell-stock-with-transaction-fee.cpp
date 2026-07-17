cc7d9cbab91019d3f4509fbc51119123f7c4e78a
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-18 00:20:17 +0530

    Time: 201 ms (5.18%), Space: 142.4 MB (5.93%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
 
+    int tabulation(const vector<int>& p, int fee){
+        int n = p.size();
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
+
+        for(int i = n-1; i>=0; i--){
+            dp[i][1] = max(-p[i]+dp[i+1][0], dp[i+1][1]);
+
+            dp[i][0] = max(p[i]-fee+dp[i+1][1], dp[i+1][0]);
+        }
+        return dp[0][1];
+    }
+
     int solve(int i, bool canBuy, int fee, const vector<int>& p, vector<vector<int>>& dp){
 
         if(i == p.size()) return 0;
@@ -25,6 +37,8 @@ public:
 
         vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
 
-        return solve(0, 1, fee, prices, dp);
+        // return solve(0, 1, fee, prices, dp);
+
+        return tabulation(prices, fee);
     }
 };
