884bdf2f4eb2e4702f3d3a2cbb04c9e230535b4e
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-17 23:37:50 +0530

    Time: 7 ms (11.02%), Space: 15.4 MB (15.6%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -2,6 +2,38 @@ class Solution {
 public:
     static constexpr int INF = 1000000000;
 
+    int tabulation(vector<vector<int>>& m){
+        int n = m.size();
+
+
+
+        vector<vector<int>> dp(n, vector<int>(n,0));
+
+        for(int j = 0; j<n; j++){
+            dp[n-1][j] = m[n-1][j];
+        }
+
+        for(int i = n-2; i>=0; i--){
+            for(int j = 0; j<n; j++){
+
+                int ans = m[i][j] + dp[i+1][j];
+
+                if(j>0) ans = min(ans, m[i][j]+dp[i+1][j-1]);
+
+                if(j<n-1) ans = min(ans, m[i][j]+dp[i+1][j+1]);
+
+                dp[i][j] = ans;
+            }
+
+        }
+
+        int ans = INF;
+        for(int j = 0; j < n; j++)
+        ans = min(ans, dp[0][j]);
+
+        return ans;
+    }
+
     int solve(int i, int j, vector<vector<int>>& m, vector<vector<int>>& dp) {
         int n = m.size();
 
@@ -30,6 +62,7 @@ public:
         for (int j = 0; j < n; j++)
             ans = min(ans, solve(0, j, matrix, dp));
 
-        return ans;
+        // return ans;
+        return tabulation(matrix);
     }
 };
