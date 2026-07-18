32557d5c80231aa481666eefe8bc08e2a6ae50bf
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-19 01:06:41 +0530

    Time: 691 ms (5.04%), Space: 343.2 MB (5.03%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
 
+    int tabulation(vector<int>& p){
+        int n = p.size();
+
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
+
+        for(int i = n-1; i>=0; i--){
+            for(int j = 0; j<=1; j++){
+                for(int k = 1; k <= 2; k++){
+
+                    if(j)
+                        dp[i][j][k] = max(-p[i] + dp[i+1][0][k], dp[i+1][1][k]);
+                    else
+                        dp[i][j][k] = max(p[i]+dp[i+1][1][k-1], dp[i+1][0][k]);
+                }
+            }
+        }
+        return dp[0][1][2];
+    }
+
     int solve(int i, int canBuy, int cap, vector<int>& p, vector<vector<vector<int>>>& dp){
 
         if(i == p.size())return 0;
@@ -25,6 +44,7 @@ public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
-        return solve(0, 1, 2, prices, dp);
+        // return solve(0, 1, 2, prices, dp);
+        return tabulation(prices);
     }
 };
