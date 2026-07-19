ee71596774add6e8a596ef5bad0f0f9b855a077c
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-19 14:56:08 +0530

    Time: 2 ms (17.88%), Space: 8.7 MB (57.91%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -3,29 +3,25 @@ public:
     string smallestSubsequence(string s) {
         vector<int> freq(26, 0);
         vector<bool> vis(26, false);
+        string ans;
 
-        for (char c : s)
-            freq[c - 'a']++;
+        for(char c : s)
+            freq[c-'a']++;
 
-        string st;
-
-        for (char c : s) {
-            freq[c - 'a']--;
-
-            if (vis[c - 'a'])
+        for(char c : s){
+            freq[c-'a']--;
+            if(vis[c-'a'])
                 continue;
 
-            while (!st.empty() &&
-                   st.back() > c &&
-                   freq[st.back() - 'a'] > 0) {
-                vis[st.back() - 'a'] = false;
-                st.pop_back();
+            while(!ans.empty()&& ans.back()>c && freq[ans.back()-'a']>0){
+                vis[ans.back()-'a'] = false;
+                ans.pop_back();
             }
 
-            st.push_back(c);
-            vis[c - 'a'] = true;
+            ans.push_back(c);
+            vis[c-'a'] = true;
         }
 
-        return st;
+        return ans;
     }
 };
