56415bc655a68781b4f0bfb11b16be5622530d51
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-20 22:58:01 +0530

    Time: 0 ms (100%), Space: 12.5 MB (62.84%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
 
+    int tabulation(vector<int>& arr, int k){
+        int n = arr.size();
+        vector<int> dp(n+1, 0);
+
+        
+        for(int i = n-1; i>=0; i--){
+            int maxi = 0;
+            for(int j = i; j<min(n, k+i); j++){
+                maxi = max(maxi, arr[j]);
+
+                dp[i] = max(dp[i], maxi*(j-i+1)+dp[j+1]);
+            }
+        }
+
+        return dp[0];
+
+    }
+
     int solve(int i, int k, vector<int>& arr, vector<int>& dp){
         int n = arr.size();
         if(i == arr.size()) return 0;
@@ -20,7 +38,9 @@ public:
     }
 
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        vector<int> dp(arr.size()+1, -1);
-        return solve(0, k, arr, dp);
+        // vector<int> dp(arr.size()+1, -1);
+        // return solve(0, k, arr, dp);
+
+        return tabulation(arr, k);
     }
 };
