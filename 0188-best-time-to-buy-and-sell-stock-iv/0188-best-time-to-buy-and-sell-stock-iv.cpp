d81649abd7caf595b8cc181246f2ce3647cbc9be
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-20 22:25:13 +0530

    Time: 3 ms (83.75%), Space: 18.3 MB (41.45%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
 
+    int tabulation(int k, vector<int>& p){
+        int n = p.size();
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1, 0)));
+
+        for(int i = n-1; i>=0; i--){
+            for(int canBuy = 0; canBuy<=1; canBuy++){
+                for(int cap = 1; cap<= k; cap++){
+                    if (canBuy) {
+                        dp[i][canBuy][cap] =
+                            max(-p[i] + dp[i+1][0][cap],
+                                dp[i+1][1][cap]);
+                    }
+                    else {
+                        dp[i][canBuy][cap] =
+                            max(p[i] + dp[i+1][1][cap-1],
+                                dp[i+1][0][cap]);
+                    }
+                }
+            }
+        }
+
+        return dp[0][1][k];
+    } 
+
     int solve(int i, bool canBuy, int cap, vector<int>& p, vector<vector<vector<int>>>& dp){
         if(i == p.size()) return 0;
         if(cap == 0) return 0;
@@ -22,8 +46,9 @@ public:
     }
 
     int maxProfit(int k, vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k+1, -1)));
-        return solve(0, 1, k, prices, dp);
+        // int n = prices.size();
+        // vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k+1, -1)));
+        // return solve(0, 1, k, prices, dp);
+        return tabulation(k, prices);
     }
 };
