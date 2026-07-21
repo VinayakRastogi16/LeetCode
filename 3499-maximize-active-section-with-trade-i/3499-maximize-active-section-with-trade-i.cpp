a339383f0073139f2b7a59178a51c6658147fd48
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-21 21:18:43 +0530

    Time: 52 ms (73.87%), Space: 79.2 MB (42.34%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -1,36 +1,74 @@
+// class Solution {
+// public:
+//     int maxActiveSectionsAfterTrade(string s) {
+        
+//         s = '1'+s+'1';
+//         vector<pair<char, int>> grp;
+
+//         for(char ch:s){
+//             if(grp.empty()||grp.back().first != ch)
+//                 grp.push_back({ch, 1});
+//             else
+//                 grp.back().second++;
+//         }
+
+//         int ones = 0;
+//         for(char ch:s){
+//             if(ch == '1')ones++;
+//         }
+
+//         ones -= 2;
+
+//         int ans = ones;
+
+//         for(int i = 1; i+1<grp.size(); i++){
+//             if(grp[i].first == '1'){
+//                 int lZero = (grp[i-1].first == '0')? grp[i-1].second:0;
+//                 int rZero = (grp[i+1].first == '0')? grp[i+1].second:0;
+
+//                 ans = max(ans, ones+lZero+rZero);
+//             }
+//         }
+
+//         return ans;
+        
+//     }
+// };
+
+
 class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
-        
-        s = '1'+s+'1';
-        vector<pair<char, int>> grp;
+        s = "1" + s + "1";
 
-        for(char ch:s){
-            if(grp.empty()||grp.back().first != ch)
-                grp.push_back({ch, 1});
-            else
-                grp.back().second++;
-        }
+        int totalOnes = -2; // ignore the two added 1's
+        int best = 0;
 
-        int ones = 0;
-        for(char ch:s){
-            if(ch == '1')ones++;
-        }
+        int prevZero = 0;
+        bool seenPrevZero = false;
 
-        ones -= 2;
+        int i = 0, n = s.size();
 
-        int ans = ones;
+        while (i < n) {
+            char ch = s[i];
+            int len = 0;
 
-        for(int i = 1; i+1<grp.size(); i++){
-            if(grp[i].first == '1'){
-                int lZero = (grp[i-1].first == '0')? grp[i-1].second:0;
-                int rZero = (grp[i+1].first == '0')? grp[i+1].second:0;
+            while (i < n && s[i] == ch) {
+                len++;
+                i++;
+            }
 
-                ans = max(ans, ones+lZero+rZero);
+            if (ch == '1') {
+                totalOnes += len;
+            } else {
+                if (seenPrevZero)
+                    best = max(best, prevZero + len);
+
+                prevZero = len;
+                seenPrevZero = true;
             }
         }
 
-        return ans;
-        
+        return totalOnes + best;
     }
 };
