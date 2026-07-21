02a82ea70a349996f7f9d90a162af431f01888b0
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-21 23:13:02 +0530

    Time: 0 ms (100%), Space: 9.8 MB (40.16%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
 
+    bool tabulation(const string& s1, const string& s2, const string& s3){
+
+
+        int n = s1.size(), m = s2.size();
+        if (n+m != s3.size()) return false;
+
+        vector<vector<int>> dp(n+1, vector<int>(m+1, false));
+        dp[n][m] = true;
+        
+
+        for(int i = n; i>=0; i--){
+            for(int j = m; j>=0; j--){
+                bool ans1 = false;
+                bool ans2 = false;
+                
+                if(i == n && j == m) continue;
+
+                if(i<n && s1[i]==s3[i+j])
+                    ans1 = dp[i+1][j];
+
+                if(j<m && s2[j]==s3[i+j])
+                    ans2 = dp[i][j+1];
+
+                dp[i][j] = ans1||ans2;
+            }
+        }
+
+        return dp[0][0];
+    }
+
     bool solve(int i, int j, const string& s1, const string& s2, const string& s3, vector<vector<int>>& dp){
         bool ans1 = false;
         bool ans2 = false;
@@ -24,7 +54,9 @@ public:
     }
 
     bool isInterleave(string s1, string s2, string s3) {
-        vector<vector<int>> dp(s1.size()+1, vector<int>(s2.size()+1, -1));
-        return solve(0, 0, s1, s2, s3, dp);
+        // vector<vector<int>> dp(s1.size()+1, vector<int>(s2.size()+1, -1));
+        // return solve(0, 0, s1, s2, s3, dp);
+
+        return tabulation(s1, s2, s3);
     }
 };
