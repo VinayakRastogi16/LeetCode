df009808b4aecd8c9b4d6a55d175f27a307b8dfe
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-07-21 20:53:37 +0530

    Time: 172 ms (8.56%), Space: 175.9 MB (11.71%) - LeetHub

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        
+        s = '1'+s+'1';
+        vector<pair<char, int>> grp;
+
+        for(char ch:s){
+            if(grp.empty()||grp.back().first != ch)
+                grp.push_back({ch, 1});
+            else
+                grp.back().second++;
+        }
+
+        int ones = 0;
+        for(char ch:s){
+            if(ch == '1')ones++;
+        }
+
+        ones -= 2;
+
+        int ans = ones;
+
+        for(int i = 1; i+1<grp.size(); i++){
+            if(grp[i].first == '1'){
+                int lZero = (grp[i-1].first == '0')? grp[i-1].second:0;
+                int rZero = (grp[i+1].first == '0')? grp[i+1].second:0;
+
+                ans = max(ans, ones+lZero+rZero);
+            }
+        }
+
+        return ans;
+        
+    }
+};
