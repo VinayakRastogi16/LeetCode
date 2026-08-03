4bec20bdf9a8954010e204d9908d49d920efef6e
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-08-03 23:39:27 +0530

    Time: 31 ms (57.34%), Space: 136.1 MB (76.04%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
 
+    int tabulation(vector<int>& st){
+        int n = st.size();
+        vector<int> dp(n+1, 0);
+
+        for(int i = n-1; i>=0; i--){
+            int ans = INT_MIN;
+            int sum = 0;
+
+            for(int k = 0; k <3&& i+k<n; k++){
+                sum +=st[i+k];
+                ans = max(ans, sum-dp[i+k+1]);
+            }
+
+            dp[i] = ans;
+        }
+
+        return dp[0];
+    }
+
     int solve(int i, vector<int>& st, int n, vector<int>& dp){
         if(i == n)
             return 0;
@@ -20,8 +39,10 @@ public:
 
     string stoneGameIII(vector<int>& stoneValue) {
         int n = stoneValue.size();
-        vector<int> dp(n, INT_MIN);
-        int diff = solve(0, stoneValue, n, dp);
+        // vector<int> dp(n, INT_MIN);
+        // int diff = solve(0, stoneValue, n, dp);
+
+        int diff = tabulation(stoneValue);
 
         if (diff > 0)
             return "Alice";
