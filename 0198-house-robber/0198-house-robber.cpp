61b9e01788db66c1995f6cc53fdf72eaabd27f5c
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-08-07 20:16:10 +0530

    Time: 0 ms (100%), Space: 10.5 MB (70.88%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
 
-
-    int tabulation(vector<int>& nums){
+    int solve(int i, vector<int>& nums, vector<int>& dp){
         int n = nums.size();
-        vector<int> dp(n+2, 0);
+        if(i >= n) return 0;
 
-        for(int i = n-1; i>=0; i--){
-            dp[i] = max(nums[i]+dp[i+2], dp[i+1]);
-        }
+        if(dp[i]!=-1) return dp[i];
 
-        return dp[0];
+        int take = nums[i] + solve(i+2, nums, dp);
+        int notTake = solve(i+1, nums, dp);
 
-    }   
-
-    int solve(vector<int>& nums, int i, vector<int>& dp){
-        if(i>=nums.size())return 0;
-
-        if(dp[i]!=-1)return dp[i];
-
-        int ans = max(nums[i]+solve(nums, i+2, dp), solve(nums, i+1, dp));
-
-        return dp[i] = ans;
+        return dp[i] = max(take, notTake);
     }
 
     int rob(vector<int>& nums) {
-
-        vector<int> dp(nums.size(), -1);
-
-        // return solve(nums, 0, dp);
-
-        return tabulation(nums);
-        
+        int n = nums.size();
+        vector<int> dp(n+1, -1);
+        return solve(0, nums, dp);
     }
 };
