26c8faf53a800a8b9f35ea1a6436e199b23e3c36
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-08-07 20:21:42 +0530

    Time: 0 ms (100%), Space: 10.6 MB (47.3%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
 
+    int tabulation(vector<int>& nums){
+        int n = nums.size();
+
+        vector<int> dp(n+2, 0);
+
+        for(int i = n-1; i>=0; i--){
+            int take = nums[i] + dp[i+2];
+            int notTake = dp[i+1];
+            dp[i] = max(take, notTake);
+        }
+
+        return dp[0];
+
+    }
+
     int solve(int i, vector<int>& nums, vector<int>& dp){
         int n = nums.size();
         if(i >= n) return 0;
@@ -15,7 +30,9 @@ public:
 
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n+1, -1);
-        return solve(0, nums, dp);
+        // vector<int> dp(n, -1);
+        // return solve(0, nums, dp);
+
+        return tabulation(nums);
     }
 };
