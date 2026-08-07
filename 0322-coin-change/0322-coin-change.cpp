a68553e410579546f66c18de8592f17dddb8526f
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-08-07 21:50:54 +0530

    Time: 17 ms (85.34%), Space: 17.9 MB (84.58%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     const int INF = 1e9;
+
+    int tabulation(vector<int>& coins, int amount){
+        vector<int> dp(amount+1, INT_MAX);
+
+        dp[0] = 0;
+
+        for(int i = 1; i<=amount; i++){
+            for(int j = 0; j<coins.size(); j++){
+                if(i>=coins[j]&&dp[i-coins[j]] != INT_MAX){
+                    dp[i] = min(dp[i], 1+dp[i-coins[j]]);
+                }
+            }
+        }
+
+        return (dp[amount] == INT_MAX)?-1:dp[amount];
+
+    }
+
     int solve(int i, vector<int>& coins, int amt, vector<vector<int>>& dp){
         if(amt == 0) return 0;
         if(i == coins.size()) return INF;
@@ -15,10 +33,12 @@ public:
     }
 
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        int ans = solve(0, coins, amount, dp);
+        // int n = coins.size();
+        // vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        // int ans = solve(0, coins, amount, dp);
 
-        return ans == INF? -1: ans;
+        int ans = tabulation(coins, amount);
+
+        return ans;
     }
 };
