29d54fa1f7fbdfc44677030048d328ce801e79e3
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-08-07 20:43:10 +0530

    Time: 63 ms (10.26%), Space: 49.9 MB (19.19%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,58 +1,24 @@
 class Solution {
 public:
-    int solve(vector<int>& coins, int amount, vector<int>& dp){
-        if(amount == 0) return 0;
+    const int INF = 1e9;
+    int solve(int i, vector<int>& coins, int amt, vector<vector<int>>& dp){
+        if(amt == 0) return 0;
+        if(i == coins.size()) return INF;
+        if(dp[i][amt] != -1)return dp[i][amt];  
 
-        if(dp[amount] != -1) return dp[amount];
+        int take = INF;
 
-        int ans = INT_MAX;
+        if(coins[i]<=amt) take = 1 + solve(i, coins, amt-coins[i], dp);
+        int notTake = solve(i+1, coins, amt, dp);
 
-        for(int i =0; i< coins.size();i++){
-            if(amount>=coins[i]){
-                int temp = solve(coins, amount - coins[i], dp);
-
-                if(temp!=INT_MAX)
-                ans = min(ans, 1+temp);
-            }
-        }
-
-        return dp[amount] = ans;
-
-    }
-
-    int coinChange2(vector<int>& coins, int amount) {
-        if(amount == 0) return 0;
-
-        vector<int> dp(amount+1, -1);
-
-        
-
-        int ans =  solve(coins, amount , dp);
-
-        if(ans == INT_MAX){
-            return -1;
-        }
-
-        return ans;
+        return dp[i][amt] = min(take, notTake);
     }
 
     int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        int ans = solve(0, coins, amount, dp);
 
-    vector<int> dp(amount + 1, INT_MAX);
-
-    dp[0] = 0;
-
-    for(int i = 1; i <= amount; i++) {
-
-        for(int j = 0; j < coins.size(); j++) {
-
-            if(i >= coins[j] && dp[i - coins[j]] != INT_MAX) {
-
-                dp[i] = min(dp[i], 1 + dp[i - coins[j]]);
-            }
-        }
+        return ans == INF? -1: ans;
     }
-
-    return (dp[amount] == INT_MAX) ? -1 : dp[amount];
-}
 };
