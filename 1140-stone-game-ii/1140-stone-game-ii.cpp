3a3695a51f5ab7829152850218a908973f3490a0
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-08-09 23:55:57 +0530

    Time: 7 ms (77.21%), Space: 13.8 MB (43.8%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -43,7 +43,7 @@ public:
     }
 
 
-    int solve(int i, int M, vector<int>& p, vector<int>& suffix, vector<vector<int>>& dp){
+    int solve(int i, int M, vector<int>& p, vector<int>& suffix, vector<vector<int>>& dp){ //Memoization
         int n = p.size();
         if(2*M>=n-i) return suffix[i];
 
@@ -63,15 +63,15 @@ public:
     }
 
     int stoneGameII(vector<int>& piles) {
-        // int n = piles.size();
-        // vector<int> suffix(n+1, 0);
-        // vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        // for (int i = n - 1; i >= 0; i--) {
-        //     suffix[i] = piles[i] + suffix[i + 1];
-        // }
-        // return solve(0, 1, piles, suffix, dp);
+        int n = piles.size();
+        vector<int> suffix(n+1, 0);
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        for (int i = n - 1; i >= 0; i--) {
+            suffix[i] = piles[i] + suffix[i + 1];
+        }
+        return solve(0, 1, piles, suffix, dp);
 
-        return tabulation(piles);
+        // return tabulation(piles);
     }
 
 
