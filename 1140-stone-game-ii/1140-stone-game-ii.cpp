bec088302328a2d66fb29af70241b6d700dfc4d6
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-08-09 23:55:32 +0530

    Time: 11 ms (54.67%), Space: 13.7 MB (43.8%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,6 +1,48 @@
 class Solution {
 public:
 
+    int tabulation(vector<int>& piles) {
+
+        int n = piles.size();
+
+        vector<int> suffix(n + 1, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            suffix[i] = piles[i] + suffix[i + 1];
+        }
+
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            for (int M = n; M >= 1; M--) {
+
+                if (2 * M >= n - i) {
+                    dp[i][M] = suffix[i];
+                    continue;
+                }
+
+                int ans = 0;
+
+                for (int X = 1; X <= 2 * M; X++) {
+
+                    int opponent =
+                        dp[i + X][max(M, X)];
+
+                    int current =
+                        suffix[i] - opponent;
+
+                    ans = max(ans, current);
+                }
+
+                dp[i][M] = ans;
+            }
+        }
+
+        return dp[0][1];
+    }
+
+
     int solve(int i, int M, vector<int>& p, vector<int>& suffix, vector<vector<int>>& dp){
         int n = p.size();
         if(2*M>=n-i) return suffix[i];
@@ -21,12 +63,16 @@ public:
     }
 
     int stoneGameII(vector<int>& piles) {
-        int n = piles.size();
-        vector<int> suffix(n+1, 0);
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        for (int i = n - 1; i >= 0; i--) {
-            suffix[i] = piles[i] + suffix[i + 1];
-        }
-        return solve(0, 1, piles, suffix, dp);
+        // int n = piles.size();
+        // vector<int> suffix(n+1, 0);
+        // vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        // for (int i = n - 1; i >= 0; i--) {
+        //     suffix[i] = piles[i] + suffix[i + 1];
+        // }
+        // return solve(0, 1, piles, suffix, dp);
+
+        return tabulation(piles);
     }
+
+
 };
