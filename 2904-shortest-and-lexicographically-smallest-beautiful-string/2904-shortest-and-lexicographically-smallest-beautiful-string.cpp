053efb83ebb27c6e6605ed294255f0d537cb9f9d
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-08-26 23:46:25 +0530

    Time: 0 ms (100%), Space: 8.8 MB (45.09%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -3,28 +3,55 @@ public:
     string shortestBeautifulSubstring(string s, int k){
         int n = s.length();
 
-        for(int len = k; len<=n; len++){
-            string r = "";
-            for(int st = 0; st<=n-len; st++){
-                string temp = s.substr(st, len);
+        // for(int len = k; len<=n; len++){
+        //     string r = "";
+        //     for(int st = 0; st<=n-len; st++){
+        //         string temp = s.substr(st, len);
 
-                int ones = 0;
-                for(char& ch:temp){
-                    ones += (ch=='1')?1:0;
+        //         int ones = 0;
+        //         for(char& ch:temp){
+        //             ones += (ch=='1')?1:0;
 
-                }
+        //         }
 
-                if(ones==k){
-                    if(r.empty()||temp<r){
-                        r = temp;
-                    }
+        //         if(ones==k){
+        //             if(r.empty()||temp<r){
+        //                 r = temp;
+        //             }
+        //         }
+        //     }
+
+        //     if(!r.empty()){
+        //         return r;
+        //     }
+        // }
+        // return "";  ####brute force
+
+        int i = 0;
+        int j = 0;
+        int cnt = 0;
+        string ans = "";
+
+        while(j<n){
+            if(s[j]=='1')
+            cnt++;
+
+            while(cnt>k||s[i]=='0'){
+                if(s[i]=='1')
+                    cnt--;
+                
+                i++;
+            }
+            
+            if(cnt==k){
+                string temp = s.substr(i, j-i+1);
+                if(ans.empty()||ans.length()>j-i+1||
+                (temp.length()==ans.length() && temp<ans)){
+                    ans = temp;
                 }
             }
-
-            if(!r.empty()){
-                return r;
-            }
+            j++;
         }
-        return "";
+        return ans;
     }
 };
