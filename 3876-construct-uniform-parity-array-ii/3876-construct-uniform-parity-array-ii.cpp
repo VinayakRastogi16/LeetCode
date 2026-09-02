ab25a311a8ce88c82d1a28f00332d60f843178fb
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-09-02 23:25:08 +0530

    Time: 213 ms (5.18%), Space: 165.8 MB (63.21%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -3,7 +3,7 @@ public:
     bool uniformArray(vector<int>& nums1) {
         int mini = INT_MAX;
         bool hasOdd = false;
-        for(int n : nums1){
+        for(int& n : nums1){
             if(n%2!=0){
                 hasOdd = true;
                 mini = min(mini, n);
@@ -14,7 +14,7 @@ public:
             return true;
         }
 
-        for(int n: nums1){
+        for(int& n: nums1){
             if(n%2==0&&n<mini){
                 return false;
             }
