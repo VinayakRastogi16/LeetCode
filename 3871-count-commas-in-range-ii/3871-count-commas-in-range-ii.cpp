f5000d87153f425baaeb3fe5171831031b40e25a
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-09-09 10:37:53 +0530

    Time: 0 ms (100%), Space: 8.9 MB (84.8%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -0,0 +1,24 @@
+#define ll long long
+class Solution {
+public:
+    long long countCommas(long long n) {
+        ll result = 0;
+        ll lower = 1000;
+        ll commas = 1;
+
+        while(lower<=n){
+            ll upper = lower*1000-1;
+
+            if(upper>n)upper = n;
+
+            ll cntNumber = upper - lower +1;
+
+            result += cntNumber*commas;
+
+            lower *= 1000;
+            commas++;
+        }
+
+        return result;
+    }
+};
