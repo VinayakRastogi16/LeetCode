79c07b4851b6eb2fa18455a6b2c4f2f4d3e9285f
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-09-17 20:44:44 +0530

    Time: 12 ms (57.99%), Space: 88.9 MB (67.53%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -1,57 +1,36 @@
 class Solution {
 public:
-    int k;
-    int n;
-
-    int solve(vector<int>& arr, int i, int count, vector<vector<int>>& dp, vector<int>& end) {
-        if (count == 0)
-            return 0;
-
-        if (i >= n)
-            return 1e9;
-
-        if(dp[i][count]!=-1)return dp[i][count];
-
-        int skip = solve(arr, i + 1, count, dp, end);
-
-        int take = 1e9;
-
-        if(end[i]!=-1){
-            int len = end[i]-i+1;
-
-            int next = solve(arr, end[i]+1, count-1, dp, end);
-
-            if(next!=1e9) take = len+next;
-        }
-
-        return dp[i][count] = min(skip, take);
-    }
-
     int minSumOfLengths(vector<int>& arr, int target) {
-        k = target;
-        n = arr.size();
+        int n = arr.size();
 
-        vector<int> end(n, -1);
+        int i = 0;
+        int j = 0;
+        int currSum = 0;
+        vector<int> minBestLenTillI(n, INT_MAX);
 
-        int l = 0;
-        long long sum = 0;
+        int bestMinLen = INT_MAX;
+        int result = INT_MAX;
 
-        for(int r = 0; r<n;r++){
-            sum += arr[r];
+        while(j<n){
+            currSum +=arr[j];
 
-            while(l<=r && sum>target){
-                sum -= arr[l];
-                l++;
+            while(i<j && currSum>target){
+                currSum -= arr[i++];
             }
 
-            if(sum == k){
-                end[l] = r;
+            if(currSum == target){
+                int len = j-i+1;
+
+                if(i>0 && minBestLenTillI[i-1] != INT_MAX){
+                    result = min(result, len+minBestLenTillI[i-1]);
+                }
+                
+                bestMinLen = min(bestMinLen, len);
             }
+            minBestLenTillI[j] = bestMinLen;
+            j++;
         }
 
-
-        vector<vector<int>> dp(n, vector<int>(3, -1));
-        int ans = solve(arr, 0, 2, dp, end);
-        return ans == 1e9 ? -1 : ans;
+        return result == INT_MAX ? -1:result;
     }
 };
