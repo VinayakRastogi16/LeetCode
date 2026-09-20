3047d541fbfe25506c31da59ec9761a64bcf3fc3
VinayakRastogi <134489958+VinayakRastogi16@users.noreply.github.com>
2026-09-20 23:16:54 +0530

    Time: 0 ms (100%), Space: 9.6 MB (46.03%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.cpp
+++ 0390-elimination-game/0390-elimination-game.cpp
@@ -3,16 +3,16 @@ public:
     int lastRemaining(int n) {
         int st = 1;
         int gap = 1;
-        bool left = true;
+        bool l = true;
 
         while(n>1){
-            if(left || n%2==1){
+            if(l || n%2==1){
                 st += gap;
             }
 
             n/=2;
             gap*=2;
-            left = !left;
+            l = !l;
         }
 
         return st;
